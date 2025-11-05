d1b86e5b22a815bd8f0355653bff4ca30a12297f
Abi7r <abi.k66722@gmail.com>
2025-11-05 22:04:23 +0530

    rb backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+
+const resumeServer = jsonServer.create();
+
+const router = jsonServer.router("db.json");
+
+const middleware = jsonServer.defaults();
+
+const PORT = 3000 || process.env.PORT;
+
+resumeServer.use(middleware);
+
+resumeServer.use(router);
+
+resumeServer.listen(PORT, () => {
+  console.log(`server started succesfully at port : ${PORT}`);
+});
